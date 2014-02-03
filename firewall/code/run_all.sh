24bc18c10011f4d1b0fbf8ab0cc8611ee73ce279
cripplet <minke.zhang@gmail.com>
2014-02-03 17:19:36 -0600

    modified run_all to run on jq submit

--- firewall/code/run_all.sh
+++ firewall/code/run_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /home/mzhang2013/courses/cmsc23010/floyd-warshall/firewall/code
+
 # make clean
 # make
 
@@ -22,7 +24,7 @@ done
 # experiment two
 for ITER in {0..4}
 do
-	for N in 1 2 4 8 16 32
+	for N in 1 2 4 8 16 # 32
 	do
 		T=`echo "(2 ^ 20) / ($N)" | bc -l`
 		echo "./firewall $T $N 1 1 $ITER 2 #2"
@@ -36,7 +38,7 @@ do
 	T=`echo "2 ^ 17" | bc -l`
 	for W in 1000 2000 4000 8000
 	do
-		for N in 1 2 4 8 16 32 64
+		for N in 1 2 4 8 16 # 32 64
 		do
 			echo "./firewall $T $N $W 1 $ITER 1 #4"
 			./firewall $T $N $W 1 $ITER 1
@@ -52,7 +54,7 @@ do
 	T=`echo "2 ^ 17" | bc -l`
 	for W in 1000 2000 4000 8000
 	do
-		for N in 1 2 4 8 16 32 64
+		for N in 1 2 4 8 16 # 32 64
 		do
 			echo "./firewall $T $N $W 0 $ITER 1 #5"
 			./firewall $T $N $W 0 $ITER 1
