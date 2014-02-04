b94007dfaadc9e70501f60645116398fada7491e
Minke Zhang <minke.zhang@gmail.com>
2014-02-03 22:05:23 -0600

    added results

---
+++ firewall/writeup/final/tabulate.py
@@ -0,0 +1,34 @@
+#!/bin/python
+
+log = open('log.txt', 'r')
+out = open('out.txt', 'r')
+
+result = {}
+
+out_lines = out.readlines()
+log_lines = log.readlines()
+
+log.close()
+out.close()
+
+for i in xrange(len(out_lines)):
+	exp = out_lines[i][:-1].split(' ')[-1][-1]
+	l = log_lines[i][:-1].split('\t')[1:]
+
+	if len(l) == 7:
+		(T, n, W, uniform, iter, mode, time) = l
+	else:
+		(T, n, W, uniform, iter, mode, time, _) = l
+
+	key = (exp, T, n, W, uniform, mode)
+	if key in result:
+		result[key] += float(time) / 5.
+	else:
+		result[key] = float(time) / 5.
+
+fp = open('result.csv', 'a')
+
+for (key, val) in result.iteritems():
+	fp.write("\t".join(str(x) for x in key) + "\t" + str(val) + "\n")
+
+fp.close()
