a475163da8571c0ac69d42b83ae6a57b1614317c
Minke Zhang <minke.zhang@gmail.com>
2014-02-03 18:38:43 -0600

    added result_t data struct

---
+++ firewall/code/result.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+#include "result.h"
+
+result *init_result() {
+	result *r = malloc(sizeof(result));
+	r->time = 0;
+	r->folded_time = 0;
+	return(r);
+}
---
+++ firewall/code/result.h
@@ -0,0 +1,11 @@
+#ifndef RESULT_H
+#define RESULT_H
+
+typedef struct result_h {
+	float time;
+	float folded_time;
+} result;
+
+result *init_result();
+
+#endif
